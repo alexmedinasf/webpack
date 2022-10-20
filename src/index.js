e5c9bd7f9e1c15a560869f51b4f254a7a936abf7
alexmedinasf <alexsales001@hotmail.com>
2022-10-20 11:35:18 -0300

    src js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,26 @@
-import _ from 'lodash';
+import '@fortawesome/fontawesome-free/css/all.css';
+import '@fortawesome/fontawesome-free/js/all.js';
+import './style.css';
+import addLi from './modules/add-li.js';
 
+const leaderboardData = {
+  result: [
+    {
+      user: 'John Doe',
+      score: 42,
+    },
+    {
+      user: 'Peter Parker',
+      score: 35,
+    },
+    {
+      user: 'Wonder Woman',
+      score: 50,
+    },
+  ],
+};
 
-function component() {
-    const element = document.createElement('div');
-  
- // Lodash, now imported by this script
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  
-    return element;
-  }
-  
-  document.body.appendChild(component());
+leaderboardData.result.forEach((element) => {
+  const text = `${element.user}: ${element.score}`;
+  addLi(text);
+});
